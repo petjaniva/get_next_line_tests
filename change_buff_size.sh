f0aa695081e42c95def327f5af7318af80df8e26
petja <petja@debian.vm>
2021-12-16 10:00:19 +0200

    form tests done? next Makefile

--- change_buff_size.sh
+++ change_buff_size.sh
@@ -1,2 +1,2 @@
 cd ..
-sed -i 's/.*BUFF_SIZE.*/# define BUFF_SIZE = '"$1"'/' get_next_line.h
+sed -i 's/.*BUFF_SIZE.*/# define BUFF_SIZE '"$1"'/' get_next_line.h
---
+++ form_tests.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   form_tests.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pniva <pniva@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/15 14:55:24 by pniva             #+#    #+#             */
+/*   Updated: 2021/12/16 09:53:22 by pniva            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "munit.h"
+#include <strings.h>
+#include <ctype.h>
+#include <limits.h>
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <assert.h>
+#include <stdio.h>
+
+void	read_fd(int fd)
+{
+	char *line;
+	
+	while (get_next_line(fd, &line) == 1)
+	{
+		if (strlen(line) == 7)
+			assert(strcmp(line, "1234567") == 0);
+		else if (strlen(line) == 15)
+			assert(strcmp(line, "abcdefghijklmno") == 0);
+		else if(strlen(line) == 3)
+			assert(strcmp(line, "123") == 0);
+		else
+			assert(0);
+	}
+	puts("OK");
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		read_fd(STDIN_FILENO);
+		return (0);
+	}
+	int fd;
+
+	fd = open(argv[1], O_RDONLY);
+	read_fd(fd);
+}
